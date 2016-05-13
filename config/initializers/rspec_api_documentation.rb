82b3441074725fd04f6ec515580d59f5df2774ea
Paulo Ancheta <paulo.ancheta@gmail.com>
2016-05-13 14:14:05 -0700

    let's add this to the rails helper instead

--- config/initializers/rspec_api_documentation.rb
+++
@@ -1,6 +0,0 @@
-# Values listed are the default values
-RspecApiDocumentation.configure do |config|
-  # Change how the post body is formatted by default, you can still override by `raw_post`
-  # Can be :json, :xml, or a proc that will be passed the params
-  config.request_body_formatter = :json
-end
